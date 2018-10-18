a520158b5a9346627ccd6f4fd38216e2eff6539e
Elsinha <35178523+elsinha@users.noreply.github.com>
2018-10-18 09:20:19 -0700

    Update feedreader.js

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -51,7 +51,7 @@ $(function() {
 
   /* Write a new test suite named "The menu" */
 
-  /* TODO: Write a test that ensures the menu element is
+  /* Write a test that ensures the menu element is
    * hidden by default. You'll have to analyze the HTML and
    * the CSS to determine how we're performing the
    * hiding/showing of the menu element.
